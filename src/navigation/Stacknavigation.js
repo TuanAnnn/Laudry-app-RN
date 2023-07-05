462a39a20ff0b8f32a67264034290d6741d65674
TuanAn <tuananthptcc10a5@gmail.com>
2023-07-06 00:54:42 +0700

    end pickup screen task add Cart screen

--- src/navigation/Stacknavigation.js
+++ src/navigation/Stacknavigation.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
+import PickupScreen from '../screens/PickupScreen';
+import CartScreen from '../screens/CartScreen';
 
 const Stack = createNativeStackNavigator();
 const Stacknavigation = () => {
@@ -10,6 +12,8 @@ const Stacknavigation = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{headerShown:false}} name="Home" component={HomeScreen} />
+        <Stack.Screen options={{headerShown:false}} name="PickUp" component={PickupScreen} />
+        <Stack.Screen options={{headerShown:false}} name="Cart" component={CartScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
