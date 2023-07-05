cc38345158e75db071c4b99831ee2af902e139cd
TuanAnnn <91050541+TuanAnnn@users.noreply.github.com>
2023-07-06 02:14:03 +0700

    add Login screen and Register screen (#3)

--- src/navigation/Stacknavigation.js
+++ src/navigation/Stacknavigation.js
@@ -1,24 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../screens/HomeScreen';
-import PickupScreen from '../screens/PickupScreen';
-import CartScreen from '../screens/CartScreen';
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import PickupScreen from "../screens/PickupScreen";
+import CartScreen from "../screens/CartScreen";
+import LoginScreen from "../screens/LoginScreen";
+import RegisterScreen from "../screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 const Stacknavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Home" component={HomeScreen} />
-        <Stack.Screen options={{headerShown:false}} name="PickUp" component={PickupScreen} />
-        <Stack.Screen options={{headerShown:false}} name="Cart" component={CartScreen} />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="PickUp"
+          component={PickupScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Cart"
+          component={CartScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Register"
+          component={RegisterScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default Stacknavigation
+export default Stacknavigation;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
